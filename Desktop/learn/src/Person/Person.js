350bdd6df20a4f5d0d0dbc3856205f04b39ea285
harrybeckwith <harryjbeckwith@gmail.com>
2021-01-21 17:32:57 +0000

    save

--- Desktop/learn/src/Person/Person.js
+++ Desktop/learn/src/Person/Person.js
@@ -1,20 +1,15 @@
-import styled from "styled-components";
 
-const StyledDiv = styled.div`
-  @media (max-width: 500px) : {
-    backgroundcolor: "blue";
-  }
-`;
+
 
 const person = (props) => {
   return (
-    <StyledDiv>
+    <div>
       <p>
         Im {props.name} and i am {props.age} years old
       </p>
       <input onChange={props.change} value={props.name} />
       <button onClick={props.click}> delete</button>
-    </StyledDiv>
+    </div>
   );
 };
 
